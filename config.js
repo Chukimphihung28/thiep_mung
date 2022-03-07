be81a0cc47b9fc19e3f4aac2b844b0c4fd469e07
Chukimphihung28 <88919147+Chukimphihung28@users.noreply.github.com>
2022-03-07 17:45:15 +0700

    Add files via upload
    crecate card

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Cẩm Tú :)';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100012697685103';
+const eventName = 'Chúc Mừng 8 - 3';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc Tú 8/3 luôn xinh đẹp, tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với Tú trong hôm nay và cả những ngày sau nhé^_^💘';
+
+
+const giftImage = 'tu.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
